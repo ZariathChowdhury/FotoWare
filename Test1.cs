b1cd4f35890a0ed9fd0477ff07c31693a2c8855e
Zariath Chowdhury <zariath.chowdhury@gmail.com>
2020-05-23 01:10:55 +0200

    Automation for test cases

---
+++ Test1.cs
@@ -0,0 +1,154 @@
+using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using WaitHelpers = SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+
+namespace FotoWare
+{
+    public class Test1
+    {
+        IWebDriver driver;
+
+        IConfigurationRoot PageProperties, TestConfig;
+
+        // Screenshot Path
+        string path = Path.Combine(
+                   Environment.GetFolderPath(
+                       Environment.SpecialFolder.Desktop
+                       ),
+                   "Screenshot",
+                   DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss")
+               );
+
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+
+            // PageProperties.json file contains Page Element Selectors
+            PageProperties = new ConfigurationBuilder()
+                .AddJsonFile("PageProperties.json")
+                .Build();
+
+            // TestConfig.json contains Test Configuration Data
+            TestConfig = new ConfigurationBuilder()
+                .AddJsonFile("TestConfig.json")
+                .Build();
+
+            // Create Directory for Screenshot Path
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+
+        [Test]
+        public void SeleniumTest()
+        {
+            WebDriverWait wait = new WebDriverWait(
+                driver, TimeSpan.FromSeconds(120)
+                );
+
+            // Go to login url
+            driver.Url = TestConfig["login_url"];
+            Console.WriteLine("Navigating to: " + driver.Url);
+
+            wait.Until(
+                WaitHelpers.
+                ExpectedConditions.
+                ElementToBeClickable(
+                    By.CssSelector(
+                        PageProperties["login_form_css"]
+                        )
+                    )
+                );
+
+            Console.WriteLine("Login Form Loaded");
+
+            // Enter Username
+            driver.PageSource.Contains(PageProperties["username_css"]);
+            IWebElement username = driver.FindElement(
+                By.CssSelector(
+                    PageProperties["username_css"]
+                    )
+                );
+            username.SendKeys(TestConfig["username"]);
+            Console.WriteLine("Entered Username.");
+
+            // Enter Password
+            driver.PageSource.Contains(PageProperties["password_css"]);
+            IWebElement password = driver.FindElement(
+                By.CssSelector(
+                    PageProperties["password_css"]
+                    )
+                );
+            password.SendKeys(TestConfig["password"]);
+            Console.WriteLine("Entered Password.");
+
+            // Click Login
+            driver.PageSource.Contains(PageProperties["login_button_css"]);
+            IWebElement loginButton = driver.FindElement(
+                By.CssSelector(
+                    PageProperties["login_button_css"]
+                    )
+                );
+            loginButton.Click();
+            Console.WriteLine("Clicked Login Button.");
+
+            // Click Archive
+            IWebElement archive = wait.Until(
+                WaitHelpers
+                .ExpectedConditions
+                .ElementToBeClickable(
+                    By.CssSelector(
+                        PageProperties["archive_css"]
+                        )
+                    )
+                );
+            archive.Click();
+            Console.WriteLine("Clicked on Archive.");
+
+            // Enter Search Keyword
+            IWebElement searchInput = wait.Until(
+                WaitHelpers
+                .ExpectedConditions
+                .ElementToBeClickable(
+                    By.Id(
+                        PageProperties["search_input_id"]
+                        )
+                    )
+                );
+            searchInput.SendKeys(TestConfig["keyword"]);
+            searchInput.SendKeys(Keys.Enter);
+            Console.WriteLine("Entered Search Keyword.");
+
+            wait.Until(WaitHelpers.ExpectedConditions
+                .UrlContains(TestConfig["keyword"]));
+
+            //Save screenshot
+            Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+            image.SaveAsFile(
+                path+"/test.png",
+                OpenQA.Selenium.ScreenshotImageFormat.Png
+                );
+            Console.WriteLine("Saved Screenshot.");
+        }
+    }
+}
---
+++ Test2.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using Microsoft.Extensions.Configuration;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FotoWare
+{
+    public class Test2
+    {
+        int number = 0;
+        List<string> searchUrls = new List<string>();
+
+
+        // PageProperties.json file contains Page Element Selectors
+        IConfigurationRoot PageProperties = new ConfigurationBuilder()
+                .AddJsonFile("PageProperties.json")
+                .Build();
+
+        // TestConfig.json contains Test Configuration Data
+        IConfigurationRoot TestConfig = new ConfigurationBuilder()
+                .AddJsonFile("TestConfig.json")
+                .Build();
+
+        // TestConfig.json contains Test Configuration Data
+        IConfigurationRoot response = new ConfigurationBuilder()
+                .AddJsonFile("response.json")
+                .Build();
+
+        [Test]
+        public string GetAuthToken()
+        {
+            var client = new RestClient(TestConfig["auth_url"]);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type","application/x-www-form-urlencoded");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("grant_type", "client_credentials");
+            request.AddParameter("client_id", TestConfig["API_id"]);
+            request.AddParameter("client_secret", TestConfig["API_secret"]);
+
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            JObject joResponse = JObject.Parse(response.Content);
+            string token = joResponse.GetValue("token").ToString();
+            Console.WriteLine(token);
+            return token;
+
+        }
+
+        public class Data
+        {
+            public string searchURL { get; set; }
+            public int assetCount { get; set; }
+        }
+
+
+        [Test]
+        public void Search()
+        {
+            var client = new RestClient(TestConfig["search_url"]);
+            var request = new RestRequest(Method.GET);
+
+            string token = GetAuthToken();
+
+            request.AddHeader("token", token);
+            request.AddParameter(
+                "q="+TestConfig["keyword"],
+                ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+
+            JArray dataArray = JArray.Parse(response.Content);
+
+            IList<Data> dataObjects = dataArray.Select(p => new Data
+            {
+                searchURL = (string)p["searchURL"],
+                assetCount = (int)p["assetCount"]
+            }).ToList();
+
+            foreach (var item in dataObjects)
+            {
+                if (item.assetCount == 1)
+                {
+                    number = +1;
+                    searchUrls.Add(item.searchURL);
+                }
+            }
+            Console.WriteLine("Number of Archive with assetCount 1: " + number);
+
+            
+        }
+
+        [Test]
+        public void GlobalSearch()
+        {
+            var request = new RestRequest(Method.GET);
+
+            for (var i = 0; i < searchUrls.Count; i++)
+            {
+                var client = new RestClient(searchUrls[i]);
+                request.AddHeader("Accept", "application/vnd.twitter-v1+json");
+                IRestResponse response = client.Execute(request);
+                JObject resp = JObject.Parse(response.Content);
+                var data = JObject.Parse(resp.ToString());
+                var result = data[0];
+                Console.WriteLine("First object in rendered array: "+ data[0]);
+            }
+
+        }
+
+    }
+}
